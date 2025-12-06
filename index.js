93d9c1cff78ffbd43d712a91c16e8ddf3df70d69
Drin Pireci <drinpireci@hotmail.com>
2025-12-06 23:12:21 +0100

    generator app

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ const bodyContainer = document.querySelector("body");
 const firstBox = document.querySelector(".first_box");
 const secondBox = document.querySelector(".second_box");
 
+const  setGradient =() => {
+  bodyContainer.style.background = `linear-gradient(to right, ${firstBox.value}, ${secondBox.value})`;
+}
 
-firstBox.addEventListener("input", () => {
-    console.log(firstBox.value)
-  bodyContainer.style.background = `linear-gradient(to right,${firstBox.value},${secondBox.value})`;
-});
-secondBox.addEventListener("input", () => {
-
-  bodyContainer.style.background = `linear-gradient(to right,${secondBox.value},${firstBox.value})`;
-});
+firstBox.addEventListener("input", setGradient);
+secondBox.addEventListener("input", setGradient);
